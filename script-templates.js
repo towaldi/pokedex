282684d031261a9fed83d4c2c9fdd458a26697bd
towaldi <tobiaswalddoerfer@gmail.com>
2023-04-08 18:55:04 +0200

    first functional pokemon card rendering

--- script-templates.js
+++ script-templates.js
@@ -1,7 +1,19 @@
 // Error message
 function templatePokeApiNotLoaded() {
     return /*html*/ `   <div class="error-message">
-                            <h1>Ooops...</h1>
+                            <h5>Ooops...</h5>
                             <p>Something went wrong while loading pokemons form the PokeAPI.</p>               
+                        </div>`;
+}
+
+// Pokemon card
+function templateRenderPokemonCard(i, currentPokemon) {
+    let pokeName = currentPokemon[i]['name'];
+    let pokeId = currentPokemon[i]['id'];
+    return /*html*/ `   <div id="pokemon-${i}" class="pokemon-card">
+                            <div class="p-c-header">
+                                <h2>${pokeName}</h2>
+                                <p>${pokeId}</p>
+                            </div>
                         </div>`;
 }
--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 // Global variables
 let allPokemons = []    // Array for API load
-let counter = 0;        // Starts counter at 0
-
+let startCounterLoading = 1;        // Start counter at 1
+let endCounterLoading = 10;         // Stop Counter at 10
 
 // Load all pokemons form PokeAPI
 async function loadPokemons() {
-    let url = `https://pokeapi.co/api/v2/pokemon/?offset=${counter}&limit=21`;      // Url of the API
-    let response = await fetch(url)     // Request to server -> loading data
-    pokemons = await response.json();   // Transfer test into JSON
-    allPokemons.push(pokemons);     // Push JSON in array `allPokemons`
-
-    console.log('loaded pokemons', allPokemons)
-
+    for (let i = startCounterLoading; i < endCounterLoading; i++) {     // Iteration to load first 9 pokemons 
+        let url = `https://pokeapi.co/api/v2/pokemon/${i}/`;      // Url of the API
+        let response = await fetch(url)     // Request to server -> loading data
+        pokemons = await response.json();   // Transfer test into JSON
+        allPokemons.push(pokemons);     // Push JSON in array `allPokemons`
+        renderPokemons(allPokemons);
+    }
 }
 
 
 // Render pokemon cards
-function renderPokemons() {
+function renderPokemons(currentPokemon) {
     let pokemonCards = document.getElementById('cards-container');      // Select `cards-container` to render all cards
     pokemonCards.innerHTML = '';    // Clear/delete container
 
-    if (allPokemons == 0) {     // If/else statement for error case: loading from API failed
-        pokemonCards.innerHTML += /*html*/ `    <div class="error-message">
-                                                    
-                                                </div>`;
+    if (allPokemons == 0) {     // If/else statement
+        pokemonCards.innerHTML += templatePokeApiNotLoaded();   // Error case -> loading from API failed
     } else {
-        
+        for (let i = 0; i < currentPokemon.length; i++) {   // Iteration -> render all pokemon cards inside array `allPokemons`
+            pokemonCards.innerHTML += templateRenderPokemonCard(i, currentPokemon)
+        }
     }
 }
