c5505d550916e3f42a31dac8d2d9d5f6d5589bd7
towaldi <tobiaswalddoerfer@gmail.com>
2023-04-09 10:33:42 +0200

    add pokemon types

--- script-templates.js
+++ script-templates.js
@@ -9,11 +9,16 @@ function templatePokeApiNotLoaded() {
 // Pokemon card
 function templateRenderPokemonCard(i, currentPokemon) {
     let pokeName = currentPokemon[i]['name'];
-    let pokeId = currentPokemon[i]['id'];
+    let pokeId = currentPokemon[i]['id'];       // Id gets converted into a string + added 0s to fill a total length of 3 (added as additional variable or inside the html code) -> let pokeIdAsString = pokeId.toString().padStart(3, '0');
+    let pokeImg = currentPokemon[i]['sprites']['other']['official-artwork']['front_default'];
     return /*html*/ `   <div id="pokemon-${i}" class="pokemon-card">
-                            <div class="p-c-header">
-                                <h2>${pokeName}</h2>
-                                <p>${pokeId}</p>
+                            <div class="row-space-between">
+                                <h5>${pokeName}</h5>
+                                <p>#${pokeId.toString().padStart(3, '0')}</p>
+                            </div>
+                            <div class="row-space-between">
+                                <p id="pokemon"></p>
+                                <img class="p-c-image" src="${pokeImg}" alt="pokemon img">
                             </div>
                         </div>`;
 }
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Global variables
 let allPokemons = []    // Array for API load
 let startCounterLoading = 1;        // Start counter at 1
-let endCounterLoading = 10;         // Stop Counter at 10
+let endCounterLoading = 21;         // Stop Counter at 10
 
 // Load all pokemons form PokeAPI
 async function loadPokemons() {
@@ -10,8 +10,8 @@ async function loadPokemons() {
         let response = await fetch(url)     // Request to server -> loading data
         pokemons = await response.json();   // Transfer test into JSON
         allPokemons.push(pokemons);     // Push JSON in array `allPokemons`
-        renderPokemons(allPokemons);
     }
+    renderPokemons(allPokemons);    // Run renderPokemons with function parameter `allPokemons` (after for loop -> execute only ones for performance reasons)
 }
 
 
@@ -26,5 +26,16 @@ function renderPokemons(currentPokemon) {
         for (let i = 0; i < currentPokemon.length; i++) {   // Iteration -> render all pokemon cards inside array `allPokemons`
             pokemonCards.innerHTML += templateRenderPokemonCard(i, currentPokemon)
         }
+    }
+}
+
+
+// Render pokemon types
+function renderPokemonTypes(i, currentPokemon) {
+    let types = document.getElementById('pokemon-types${i}');   // Select `pokemon-types` at position `i` to render the 1 or 2 types
+    let pokemon = currentPokemon[i];    // `currentPokemon` at position `i` in array is assigned to the variable `pokemon`
+    for (let i = 0; i < pokemon.length; i++) {     // Iteration through the array `pokemon`
+        const type = pokemon['types'][i]['type']['name'];
+        types.innerHTML += ``;
     }
 }
